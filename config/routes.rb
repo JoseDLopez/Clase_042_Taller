8cd0ad3c95f9e58c7e3fbc66f679894a858f3258
JoseDLopez <josed-lopez@hotmail.com>
2016-10-18 02:30:04 -0300

    Se agregan los dos tipos de form, get y post, y se toma el screenshot del movimiento de la base de datos

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'pages/guardar_cliente'
 
-  get 'pages/guardar_auto'
+  post 'pages/guardar_auto'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
