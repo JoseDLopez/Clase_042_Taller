0cc5ee88e11725a68a0bc4287dab790e1f6d3584
JoseDLopez <josed-lopez@hotmail.com>
2016-10-18 01:36:11 -0300

    Se agrega base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	@nombre_completo = params[:nombre_completo]
+  	@correo_electronico = params[:correo_electronico]
+  	@edad_usuario = params[:edad_usuario]
+    @clientes = Client.all
   end
 
   def index
@@ -10,4 +14,20 @@ class PagesController < ApplicationController
 
   def x
   end
+
+  def guardar_cliente
+    @nombre_completo = params[:nombre_completo]
+    @correo_electronico = params[:correo_electronico]
+    @edad_usuario = params[:edad_usuario]
+
+    Client.create({
+      name: @nombre_completo,
+      email: @correo_electronico,
+      edad: @edad_usuario
+      })
+
+    redirect_to pages_home_path
+  end
+
+
 end
